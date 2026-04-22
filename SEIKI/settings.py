3ceef8dab74efd0dbd01f50bf4178478210a436a
gmaravillaa <gmaravilla@gbox.adnu.edu.ph>
2026-04-23 03:44:31 +0800

    fix: add render.com domains to default ALLOWED_HOSTS

--- SEIKI/settings.py
+++ SEIKI/settings.py
@@ -28,7 +28,7 @@ load_dotenv(dotenv_path=env_path, override=True)
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,*.onrender.com').split(',')
 
 
 # Application definition
