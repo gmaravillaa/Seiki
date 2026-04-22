9dd06802e18ea6e59fe6417f3f997e4dad288070
gmaravillaa <gmaravilla@gbox.adnu.edu.ph>
2026-04-22 10:51:54 +0800

    Remove tracked files that should be ignored

--- SEIKI/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class SeikiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'SEIKI'
-
-    def ready(self):
-        import SEIKI.signals
