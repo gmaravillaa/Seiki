6fe848a7148612866dde9cecb485d856a31f8a7c
Cholsss <153795318+Cholsss@users.noreply.github.com>
2026-04-27 16:07:12 +0800

    Update urls.py

--- SEIKI/urls.py
+++ SEIKI/urls.py
@@ -21,6 +21,7 @@ from django.contrib.auth.views import LogoutView, LoginView
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
 
     # =======================
@@ -32,7 +33,7 @@ urlpatterns = [
     # AUTH
     # =======================
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='index'), name='logout'),
 
     # =======================
     # ROOT
